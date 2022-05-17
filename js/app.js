9f08973853e3b3033106a9a82285038453241aae
nmartoia <nmartoia@edenschool.fr>
2022-05-17 12:05:03 +0200

    finish project

--- js/app.js
+++ js/app.js
@@ -1,26 +1,41 @@
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
-const p = document.createElement("p")
+const h1 = document.createElement("h1")
 const div = document.querySelector("div")
 const btn = document.querySelector("button")
 const input = document.querySelector("input")
 const mot = "chocolat"
-const tmb=[]
-const t=[]
-let i =0
-let n=0
-while(i<mot.length){
-    let random=getRandomInt(mot.length);
-    let rmot = mot[random];
-    if(!tmb.includes(rmot)||!t.includes(random)){
-        tmb.push(rmot)
-        t.push(random)
-        i++
+function mrandom(m){
+    const tmb=[]
+    const t=[]
+    let i =0
+    while(i<m.length){
+        let random=getRandomInt(m.length);
+        let rmot = m[random];
+        if(!tmb.includes(rmot)||!t.includes(random)){
+            tmb.push(rmot)
+            t.push(random)
+            i++
+        }
+    }
+    let verif=[]
+    for (let i = 0; i < tmb.length; i++) {
+        if(tmb[i]==m[i]){
+            verif.push(i)
+        }
+    }
+    if(verif.length<m.length/4){
+        return tmb
+    }
+    else{
+        return mrandom(mot) 
     }
 }
-p.textContent=tmb.join("")
-div.appendChild(p)
+let random = mrandom(mot)
+console.log(random)
+h1.textContent=random.join("")
+div.appendChild(h1)
 btn.addEventListener("click",()=>{
     if(input.value==mot){
         alert("vous avez gagner")
